df6a108db0fe6eaf17589bfbd3f89fcf45fb4b13
HunterYim <129152424+HunterYim@users.noreply.github.com>
2024-11-27 12:46:29 +0900

    Update transactions.js
    상단 버튼 스크립트 분리

--- public/transactions.js
+++ public/transactions.js
@@ -87,36 +87,6 @@ function loadSales() {
   loadSales();
 });
 
-
-//상당 아이콘 이동
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("home-btn").addEventListener("click", () => {
-      window.location.href = "home.html"; // Home 페이지로 이동
-    });
-  document.getElementById("profile-btn").addEventListener("click", () => {
-    window.location.href = "profile.html"; // Profile 페이지로 이동
-  });
-  document.getElementById("board-btn").addEventListener("click", () => {
-    window.location.href = "board.html"; // Board 페이지로 이동
-  });
-  document.getElementById("notification-btn").addEventListener("click", () => {
-    window.location.href = "transactions.html"; // Transactions 페이지로 이동
-  });
-  document.getElementById("logout-btn").addEventListener("click", () => {
-    // 로그아웃 처리 로직
-    fetch('/logout', { method: 'POST' })
-      .then(response => {
-        if (response.ok) {
-          window.location.href = "login.html"; // 로그아웃 후 로그인 페이지로 이동
-        } else {
-          console.error("로그아웃 실패");
-        }
-      })
-      .catch(error => console.error("로그아웃 오류:", error));
-  });
-});
-
-
 // 사용자 목록 가져오기
 window.onload = function () {
   // 사용자 정보 불러오기
