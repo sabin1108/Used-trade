427f94084e62eb1a3b352b55e41a6250283de23a
HunterYim <129152424+HunterYim@users.noreply.github.com>
2024-11-27 12:35:23 +0900

    Update profile.js
    상단 버튼 작동 스크립트 분리

--- public/profile.js
+++ public/profile.js
@@ -57,38 +57,9 @@ document.addEventListener("DOMContentLoaded", function() {
       .catch(error => console.error("채팅 목록 로드 오류:", error));
   }
 
-
     // 초기 데이터 로드
     loadChatList();
 
-});
-
-//상당 아이콘 이동
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("home-btn").addEventListener("click", () => {
-    window.location.href = "home.html"; // Home 페이지로 이동
-  });
-  document.getElementById("profile-btn").addEventListener("click", () => {
-    window.location.href = "profile.html"; // Profile 페이지로 이동
-  });
-  document.getElementById("board-btn").addEventListener("click", () => {
-    window.location.href = "board.html"; // Board 페이지로 이동
-  });
-  document.getElementById("notification-btn").addEventListener("click", () => {
-    window.location.href = "transactions.html"; // Transactions 페이지로 이동
-  });
-  document.getElementById("logout-btn").addEventListener("click", () => {
-    // 로그아웃 처리 로직
-    fetch('/logout', { method: 'POST' })
-      .then(response => {
-        if (response.ok) {
-          window.location.href = "login.html"; // 로그아웃 후 로그인 페이지로 이동
-        } else {
-          console.error("로그아웃 실패");
-        }
-      })
-      .catch(error => console.error("로그아웃 오류:", error));
-  });
 });
 
 // 새 게시글 작성 버튼 클릭 시 board.html로 이동
